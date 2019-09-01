cae979bfc7ef30b16722fab6d48150dcb0b6d98f
Carolin Brandt <carolin.brandt@tum.de>
2019-09-01 12:07:03 +0200

    wip commander.js test

---
+++ cli.js
@@ -0,0 +1,64 @@
+const program = require('commander');
+var pjson = require('./package.json');
+
+program
+  .version(pjson.version)
+  .option('-b, --bot-output', 'format output to use in telegram bot');
+
+program
+  .command('tree [n]')
+  .description('print your workflowy nodes up to depth n (default: 2)')
+  .option('-i, --id <id/alias>', 'print sub nodes under the <id> (default: whole tree)', '')
+  .option('-n, --withnote', 'print the note of nodes (default: false)', false)
+  .option('-c, --hiddencompleted', 'hide the completed lists (default: false)', false)
+  .option('-w, --withid', 'print id of nodes (default: false)', false)
+  .option(', ', '')
+  .action(function(env, options){
+    const mode = options.setup_mode || 'normal';
+    env = env || 'all';
+    console.log('setup for %s env(s) with %s mode', env, mode);
+  });
+
+program
+  .command('capture')
+  .description('add something to a particular node')
+  .option('-p, --parentid <id/alias>', '36-digit uuid of parent (required) or defined alias')
+  .option(', --name <str>', 'what to actually put on the node (required)')
+  .option(', --priority=<int>', '0 as first child, 1 as second (default 0 (top)), (use a number like 10000 for bottom)', 0)
+  .option(', --note=<str>', 'a note for the node (default "")', '')
+  .action(function(env, options){
+
+  });
+
+program
+  .command('alias')
+  .description()
+  .option(', ', '')
+  .action(function(env, options){
+
+  });
+
+program
+  .command('')
+  .description()
+  .option(', ', '')
+  .action(function(env, options){
+
+  });
+
+program
+  .command('exec <cmd>')
+  .alias('ex')
+  .description('execute the given remote cmd')
+  .option('-e, --exec_mode <mode>', 'Which exec mode to use')
+  .action(function(cmd, options){
+    console.log('exec "%s" using %s mode', cmd, options.exec_mode);
+  }).on('--help', function() {
+    console.log('');
+    console.log('Examples:');
+    console.log('');
+    console.log('  $ deploy exec sequential');
+    console.log('  $ deploy exec async');
+  });
+
+program.parse(process.argv);
